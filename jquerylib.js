b7e37f1e6cdfd45806399fb7174419a16f13b070
NoaUnity <noassock@gmail.com>
2020-10-26 14:16:53 +0100

    add jquery personal function library

---
+++ jquerylib.js
@@ -0,0 +1,215 @@
+/*
+
+$("p").hide();
+
+
+$("#test").hide();
+
+
+$(".test").hide();
+
+
+$("*").hide();
+
+
+$("[href]").hide();
+
+
+$("tr:odd").hide();
+
+
+$("p").click(function(){
+  $(this).hide();
+});
+
+
+$("p").dblclick(function(){
+  $(this).hide();
+});
+
+
+$("p").mouseenter(function(){
+  $(this).hide();
+});
+
+
+$("input").keypress(function(){
+  $(this).hide();
+});
+
+
+$("p").on("click", function(){
+  $(this).hide();
+});
+
+
+$("p").click(function(){
+  $(this).hide();
+});
+
+
+$("p").click(function(){
+  $(this).hide("slow");
+});
+
+
+$("button").click(function(){
+  $("p").show();
+});
+
+
+$("button").click(function(){
+  $("p").toggle();
+});
+
+
+$("div").fadeOut();
+
+
+$("div").fadeOut("slow");
+
+
+$("div").fadeTo("slow", 0.2);
+
+
+$("button").click(function(){
+  $("div").fadeToggle(1000);
+});
+
+
+$("div").slideUp();
+
+
+$("div").slideUp("slow");
+
+
+$("div").slideDown();
+
+
+$("button").click(function(){
+  $("div").slideToggle();
+});
+
+
+$("div").animate({left: '250px'});
+
+
+$("div").animate({height: '500px'});
+
+
+$("div").animate({
+opacity: '0.4', 
+height: '500px', 
+width: '500px'
+});
+
+$("div").animate({fontSize: '100px'}, "slow");
+
+
+$("div").stop();
+
+
+$("div").text();
+
+
+$("div").html();
+
+
+$("input").val();
+
+
+$("a").attr("href");
+
+
+$("div").text("Hello World");
+
+
+$("input").val("John Doe");
+
+
+$("img").attr("src", "myimage.jpg");
+
+
+$("#myLink").text("Demo");
+$("#myLink").attr("href", "https://www.demo.com");
+
+
+$("p").append("YES!");
+
+
+$("p").prepend("YES!");
+
+
+$("div").remove();
+
+
+$("div").empty();
+
+
+$("div").remove(".test, .demo");
+
+
+$("p").addClass("important");
+
+
+$("p").addClass("important test");
+
+
+$("p").removeClass("important");
+
+
+$("p").click(function(){
+  $(this).toggleClass("important");
+});
+
+
+$("p").css("background-color", "pink");
+
+
+$("p").css("border", "5px dotted red");
+
+
+$("p").css("background-color");
+
+
+$("p").css({
+  "background-color": "green",
+  "color": "white",
+  "font-size": "25px",
+  "padding": "15px"
+});
+
+
+$("div").height(500).width(500);
+
+
+$("div").height()
+
+
+$("div").innerHeight()
+
+
+$("div").outerHeight()
+
+
+$("div").outerHeight(true)
+
+
+$("span").parent();
+
+
+$("span").parents();
+
+
+$("div").children();
+
+
+$("div").find("span");
+
+
+$("h2").siblings();
+
+
+$("div").first();
+
+*/
